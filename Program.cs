a84aca98f7400b4b0a2afff8966578c1304f87c5
uzkid <ibowik694@gmail.com>
2024-08-07 13:58:05 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+System.Console.Write("Hello Please Enter Your Birth Year: ");
+int UserAgeAsYear = System.Convert.ToInt32(Console.ReadLine());
+int Year = 2024;
+
+int UserAge = Year - UserAgeAsYear;
+Console.WriteLine($"your age is {UserAge}");
+
+int UserAgeInDays = UserAge * 365;
+
+Console.WriteLine($"That means you lived {UserAgeInDays} Days On Earth");
